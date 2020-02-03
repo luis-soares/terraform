e4901e17823998a36c921edab734b853d0e7d85b
luissoares <40070907+luis-soares@users.noreply.github.com>
2020-02-03 20:05:42 -0300

    Update vars.tf

--- vcenter/modules/winvm/vars.tf
+++ vcenter/modules/winvm/vars.tf
@@ -1,8 +1,21 @@
+# Luis Antonio Soares da Silva (lui_eu@msn.com / luissoares@outlook.com) 
 
-
+# VCENTER VARS
 variable "vsphere_user" { type = string}
 variable "vsphere_password" { type = string }
 variable "vsphere_server" { type = string }
+
+# ENV VCENTER
+variable "datacenter" { type = string }
+variable "vmcluster" { type = string }
+
+# NETWORK VARS
+variable "dswitchid" { type = string }
+variable "vlanname" { type = string }
+variable "vlanid" { type = string }
+
+#OS VARS
+
 variable "name" { type = string }
 variable "ipaddr" { type = string }
 variable "cidr" { type = string }
@@ -11,12 +24,12 @@ variable "dns_suffixes" { type = string }
 variable "vcpu" { type = string }
 variable "memory" { type = string }
 variable "disksize" { type = string}
-variable "dswitchid" { type = string }
-variable "vlanname" { type = string }
-variable "vlanid" { type = string }
-variable "datacenter" { type = string }
 variable "datastore" { type = string }
 variable "vm_template" { type = string }
-variable "vmcluster" { type = string }
 variable "firmware" { type = string }
 variable "resourcepoolname" { type = string }
+
+
+
+
+
